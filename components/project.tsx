dd4843a752f47affcd0b1dcb0c4a575fe34fd3ec
Cristopher Canales <cristopher.txt@gmail.com>
2023-11-04 15:21:54 -0600

    actualizacion de la experiencia laboral

--- components/project.tsx
+++ components/project.tsx
@@ -38,7 +38,7 @@ export default function Project({
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto mb-2">
             {tags.map((tag, index) => (
               <li
                 className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
